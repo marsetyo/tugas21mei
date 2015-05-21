b1bd868db5fb2119316c7b849a47f4d2ed479b20
marsetyo <marsetyo@Marsetyos-MacBook-Pro-2.local>
2015-05-21 08:40:47 +0700

    6th commit, memasangkan arrayadapter dengan listview

--- app/src/main/java/com/kelompokamalia/wowtechno/MainActivity.java
+++ app/src/main/java/com/kelompokamalia/wowtechno/MainActivity.java
@@ -44,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         listOfBook.add("Tutorial Pemrograman Android");
 
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,listOfBook);
+
+        listViewBook.setAdapter(adapter);
     }
 
 
